07d12c765c28eb27740921ab79d395340f96f6c7
Shekelme <aleksei.shcherbina@yandex.ru>
2022-05-26 16:57:17 +0700

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -26,21 +26,41 @@ fi
 echo $$ > /var/run/ironfish/${filename}.pid
 
 dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
+dpkg -s parallel > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install parallel > /dev/null 2>&1; echo "Note: parallel package has been installed"; fi
 
 while true; do
+INSUFFICIENT_COUNT=0
 # BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')
 BALANCE=`ironfish accounts:balance | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//'`
+echo ${BALANCE} > /tmp/.shadow_balance
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	REPEAT=$(echo ${BALANCE}/0.10000001 | bc -l | cut -d '.' -f1)
 	if [ ! -z "${REPEAT}" ]; then
 		for i in `seq ${REPEAT}`; do
+			if [ "$(($i % 10))" == 0 ] && [ "$i" != "1" ]; then
+				echo $(ironfish accounts:balance | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//') > /tmp/.shadow_balance 2>&1 &
+			fi
+			if (( $(echo $(cat /tmp/.shadow_balance) \>\= 0.10000001 | bc -l) )) && [ "$i" != "1" ]; then
+				echo -e $(date): '\033[1;32m'Possible balance amount is about $(echo $(cat /tmp/.shadow_balance)-\(${i}-1\)*0.10000001 | bc | sed "/^\./ s/\./0\./")'\033[0m'
+			fi
 			echo -e '\033[1;32m'"Transaction:"'\033[0m'
 			ironfish deposit --confirm | tee /tmp/deposit-last.log
 			echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
-			sleep 90
 			if [ ! -z "$(egrep -i "Insufficient" /tmp/deposit-last.log)" ]; then
-				sleep 120
+				((INSUFFICIENT_COUNT++))
+				echo "Insufficient funds error count is ${INSUFFICIENT_COUNT}"
+					if [ "${INSUFFICIENT_COUNT}" == "5" ] && [ -z "$(ps aux | egrep "accounts:rescan" | egrep -v grep | grep ironfish)" ] && [ "${1}" == "rescan-allowed" ]; then
+						echo -e '\033[0;31m'Too many Insufficient funds errors. Rescan will start now.'\033[0m'
+						ironfish accounts:rescan
+						INSUFFICIENT_COUNT=0
+						break
+					fi
+				sleep 600
+			fi
+			if [ ! -z "$(egrep -i "An error occurred while sending the transaction" /tmp/deposit-last.log)" ]; then
+				# It means that network is down, script will sleep for 30 minutes until next try
+				sleep 1800
 				break
 			fi
 		done
